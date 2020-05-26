492ee1ca9b3ef13d897c5c20c97f699115948bf1
Ricardo Salim <ricardosalim7@hotmail.com>
2020-05-26 15:20:16 -0300

    heroku

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const nomeApp = process.env.npm_package_name;
+const app = express();
+const cors = require('cors');
+ 
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+app.use(cors());
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+});
+ 
+app.listen(process.env.PORT || 8080);
