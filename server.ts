953520019c3d08771318671321311d8cb9ac49cd
Ricardo Salim <ricardosalim7@hotmail.com>
2020-05-26 16:03:24 -0300

    heroku

--- server.ts
+++ server.ts
@@ -1,21 +1,12 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
 const app = express();
 const cors = require('cors');
- 
-app.use(cors());
-app.options('*', cors())
-app.use(express.static(`${__dirname}/dist/${nomeApp}`)); 
-app.get('/*', cors(), function(req,res) {
 
-    if(req.url === '/') {
-      res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
-    }
-    else {
-      res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/` + req.url))
-    }
-  
-  });
- 
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/dist/poke-challenge'));
+
+app.get('/*', cors(),function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/poke-challenge/index.html'));
+});
+
+app.listen(process.env.PORT || 3000);
